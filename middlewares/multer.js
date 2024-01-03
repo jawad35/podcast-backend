c565f772b3beb3702ffdadf86d2be563e89fe5d4
jawad35 <jaqimughal@gmail.com>
2024-01-03 13:30:09 +0500

    Batch6

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,12 +3,11 @@ const multer = require('multer');
 // set storage
 var storage = multer.diskStorage({
     destination : function ( req , file , cb ){
-        console.log(req)
         cb(null, 'uploads')
     },
     filename : function (req, file , cb){
         const ext = file.originalname.substr(file.originalname.lastIndexOf('.'));
-        cb(null, `http://207.180.232.109:8003/uploads/${file.originalname.replace(/\s/g, '')}-${req.query.randomId}${ext}`)
+        cb(null, `${file.originalname.replace(/\s/g, '')}-${req.query.randomId}${ext}`)
     }
 })
 
